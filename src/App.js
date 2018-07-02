dd0ad0cadae4e1471946604e1d679643390b3466
Vismaya T Surendran <vismayatsurendran@gmail.com>
2018-07-02 15:32:12 +0530

    Added locations and markers

--- src/App.js
+++ src/App.js
@@ -2,6 +2,12 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+	state={
+		allPlaces:require("./locations.json"),
+		map:"",
+		prev:[],
+		markers:[]
+	}
 
 	componentDidMount=()=> {
 	window.initMap=this.initMap;
@@ -16,7 +22,47 @@ class App extends Component {
 		center:{lat:11.259550, lng:75.776057},
 		zoom:15,
 	});
+	this.setState({
+      map: map,
+    });
+
+   var locationsList=[];
+   var bounds= new window.google.maps.LatLngBounds();
+
+    this.state.allPlaces.forEach(place=>{
+    var marker = new window.google.maps.Marker({
+    	position: {
+                	lat: place.latitude, 
+                	lng: place.longitude},
+                	map: map,
+                	animation: window.google.maps.Animation.DROP,
+                	fullname : place.name + ' -- ' + place.type
+   })
+    locationsList.push(marker);
+    var locations= new window.google.maps.LatLng(marker.position.lat(), marker.position.lng());
+    bounds.extend(locations);
+    window.google.maps.event.addListener(marker,'click',()=> {
+        this.openInfo(marker);
+      })
+    })
+   this.setState({
+  		prev:locationsList,
+  		markers:locationsList
+  	})
+   var center=map.getCenter();
+
+   window.google.maps.event.addDomListener(window, "resize", ()=> {
+   	this.state.map.setCenter(center);
+   	this.state.map.fitBounds(bounds);
+    this.state.map.panToBounds(bounds);
+});
+
+   var infowindow=new window.google.maps.InfoWindow({maxWidth:250});
+	this.setState({
+		infowindow:infowindow
+	})
 }
+
   render() {
     return (
       <div>
